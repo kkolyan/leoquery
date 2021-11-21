f24a797e306a02fadf3e88cac45e0c314e94cc1d
Nikolay Plekhanov <kkolyankkolyan@gmail.com>
2021-11-21 14:50:14 +0300

    test added to asset AutoReset behavior

--- test/CompInitTest.cs
+++ test/CompInitTest.cs
@@ -1,8 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 using Kk.LeoQuery;
+using Leopotam.EcsLite;
 using NUnit.Framework;
 
 namespace leoquery.test
@@ -52,14 +52,74 @@ namespace leoquery.test
             
             Assert.IsFalse(weakList.IsAlive);
         }
+        
+        [Test]
+        public void ClearedAfterWithAutoReset()
+        {
+            IEntityStorage storage = new LeoLiteStorage();
+            WeakReference<List<int>> weakList;
+            {
+                Entity e1 = storage.NewEntity();
+                C2 a = e1.Add<C2>();
+                a.list.Add(42);
+                weakList = new WeakReference<List<int>>(a.list);
+                e1.Destroy();
+                
+                // for some reason scope end doesn't free it, so we need to nullify it manually
+                a = default;
+                e1 = default;
+            }
+            
+            GC.Collect();
+            
+            Assert.IsTrue(weakList.TryGetTarget(out var list));
+            Assert.AreEqual(0, list.Count);
+        }
+
+        [Test]
+        public void ScalarsClearedWithAutoInit()
+        {
+            IEntityStorage storage = new LeoLiteStorage();
+            Entity a = storage.NewEntity();
+            a.Add<C1>().scalar = 42;
+            a.Destroy();
+
+            Entity b = storage.NewEntity();
+            Assert.AreEqual(0, b.Add<C1>().scalar);
+        }
+
+        [Test]
+        public void ScalarsNotClearedWithAutoReset()
+        {
+            IEntityStorage storage = new LeoLiteStorage();
+            Entity a = storage.NewEntity();
+            a.Add<C2>().scalar = 42;
+            a.Destroy();
+
+            Entity b = storage.NewEntity();
+            Assert.AreEqual(42, b.Add<C2>().scalar);
+        }
 
         public struct C1 : IComponentInit<C1>
         {
             public List<int> list;
+            public int scalar;
 
             public void Init(ref C1 c)
             {
                 c.list = new List<int>();
+            }
+        }
+
+        public struct C2 : IEcsAutoReset<C2>
+        {
+            public List<int> list;
+            public int scalar;
+
+            public void AutoReset(ref C2 c)
+            {
+                c.list ??= new List<int>();
+                c.list.Clear();
             }
         }
     }
